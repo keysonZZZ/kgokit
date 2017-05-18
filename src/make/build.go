a3b08c8ddb99e93625a01cfba9bff8a1c06d7846
keyson <keysonzzz@163.com>
2017-05-18 17:09:54 +0800

    save

--- src/make/build.go
+++ src/make/build.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"github.com/keysonZZZ/kgo/kgoGoSource"
 	"github.com/keysonZZZ/kgo/kgoCmd"
+	"github.com/keysonZZZ/kgo/kgoGoSource"
 )
 
 func buildCmd() {
 	kgoGoSource.MustGoInstallWithDefaultEnv("github.com/bronze1man/kmg/kmg")
 	kgoCmd.ProxyRun("cp -f ./bin/kmg /usr/local/bin/kmg")
 }
-
