0d9efb2d01244b6b82bba9e1fb4dc1db0f2244e8
keyson <keysonzzz@163.com>
2017-06-23 10:57:18 +0800

    save

--- src/make/Unit_test.go
+++ src/make/Unit_test.go
@@ -1,3 +1,10 @@
 package make
 
+import (
+	"testing"
+	"github.com/keysonZZZ/kgo/kgoRpc/kgoRpcGoAndJava/kgoGajTest/kgoGajTestBuild"
+)
 
+func TestBuildJavaRpc(t *testing.T) {
+	kgoGajTestBuild.TestBuild()
+}
